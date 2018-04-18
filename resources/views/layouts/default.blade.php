05866baff2264a0d46cf6c93319fd6189b942090
zgf123 <277845885@qq.com>
2018-04-18 09:14:13 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>@yield('title', 'Laravel')</title>
-</head>
-<body>
-	@yield('content')
-</body>
+<html>
+  <head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('layouts._header')
+
+    <div class="container">
+      @yield('content')
+      @include('layouts._footer')
+    </div>    
+  </body>
 </html>
