ece5c375be1659dd5a06c4e5e952e05c1d0fc172
zgf123 <277845885@qq.com>
2018-04-18 06:33:18 +0000

    ___

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    第一个应用
                 </div>
 
                 <div class="links">
