3fcace59b449be9de87a922f3cc46131d2402bff
zgf123 <277845885@qq.com>
2018-05-09 10:03:13 +0000

    Add user statuses

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call(UsersTableSeeder::class);
+        $this->call(StatusesTableSeeder::class);
 
         Model::reguard();
     }
